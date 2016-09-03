7fb163fa87165c89184d32f1e90dbba5c938e9d3
ARGEL JOSEPH FIGUEROA ESCOBAR <argel.joseph.figueroa.escobar@ericsson.com>
2016-09-03 04:15:32 -0600

    se agrego la creacion del esquema

--- db/sat_script.sql
+++ db/sat_script.sql
@@ -17,14 +17,18 @@ USE `sat` ;
 -- -----------------------------------------------------
 -- Table `sat`.`REMESAS`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `sat`.`REMESAS` ;
+
 CREATE TABLE IF NOT EXISTS `sat`.`REMESAS` (
-  `ID_REMESAS` INT NOT NULL,
+  `ID_REMESAS` INT NOT NULL AUTO_INCREMENT,
   `FECHA_RECEPCION` DATETIME NULL,
   `FECHA_PAGO_IMPUESTO` DATETIME NULL,
-  `MONTO` FLOAT NULL,
+  `MONTO` DOUBLE NULL,
   `usuario` VARCHAR(10) NULL,
   `contrasena` VARCHAR(10) NULL,
   `correlativoRemesa` INT NULL,
+  `montoPagado` DOUBLE NULL,
+  `montoTasaCambio` DOUBLE NULL,
   PRIMARY KEY (`ID_REMESAS`))
 ENGINE = InnoDB;
 
